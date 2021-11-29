417f275e745a3f5fb4322a4654a7810e590e8422
Jeremy Borgman <borgman.jeremy@pm.me>
2021-11-29 11:44:29 -0600

    removing monsters

--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -1,5 +1,6 @@
 use crate::prelude::*;
 
+mod collision;
 mod entity_render;
 mod map_render;
 mod player_input;
@@ -7,6 +8,7 @@ mod player_input;
 pub fn build_scheduler() -> Schedule {
     Schedule::builder()
         .add_system(player_input::player_input_system())
+        .add_system(collision::collisions_system())
         .add_system(map_render::map_render_system())
         .add_system(entity_render::entity_render_system())
         .build()
