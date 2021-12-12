fa5cc0ee4aac20e2af02f99db9c4011aa815b1a0
Jeremy Borgman <borgman.jeremy@pm.me>
2021-12-11 20:46:25 -0600

    cellular generation

--- src/map_builder/mod.rs
+++ src/map_builder/mod.rs
@@ -1,6 +1,10 @@
 use crate::prelude::*;
+use automata::CellularAutomataArchitect;
 use empty::EmptyArchitect;
+use rooms::RoomsArchitect;
+mod automata;
 mod empty;
+mod rooms;
 const NUM_ROOMS: usize = 20;
 
 trait MapArchitect {
@@ -109,8 +113,42 @@ impl MapBuilder {
         }
     }
 
+    fn spawn_monsters(
+        &self,
+        start: &Point,
+        rng: &mut RandomNumberGenerator,
+    ) -> Vec<Point> {
+        const NUM_MONSTERS: usize = 50;
+
+        let mut spawnable_tiles: Vec<Point> = self
+            .map
+            .tiles
+            .iter()
+            .enumerate()
+            .filter(|(idx, t)| {
+                **t == TileType::Floor
+                    && DistanceAlg::Pythagoras
+                        .distance2d(*start, self.map.index_to_point2d(*idx))
+                        > 10.0
+            })
+            .map(|(idx, _)| self.map.index_to_point2d(idx))
+            .collect();
+
+        let mut spawns = Vec::new();
+
+        for _ in 0..NUM_MONSTERS {
+            let target_index =
+                rng.random_slice_index(&spawnable_tiles).unwrap();
+
+            spawns.push(spawnable_tiles[target_index]);
+            spawnable_tiles.remove(target_index);
+        }
+
+        spawns
+    }
+
     pub fn new(rng: &mut RandomNumberGenerator) -> Self {
-        let mut architect = EmptyArchitect {};
+        let mut architect = CellularAutomataArchitect {};
         architect.new(rng)
     }
 }
